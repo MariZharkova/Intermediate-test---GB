d2f2b13dd7d64dd0d0e45f96e66a66117cce1201
Marina Zharkova <muzharkova@gmail.com>
2024-02-08 22:49:06 +0400

    toy store added

--- Java/ToyStore.java
+++ Java/ToyStore.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class ToyStore {
+import java.util.*;
+
+class Toy {
+    String name;
+    Integer weight;
+
+    public Toy(String n, Integer w) {
+        name = n;
+        weight = w;
+    }
+}
+
+class ParsingResult {
+    Integer id;
+    String name;
+    Integer weight;
+}
+
+public class ToyStore {
+    // ID -> Toy mapping
+    private HashMap<Integer, Toy> toys;
+    public ToyStore (List<String> toysList) {
+        toys = new HashMap<Integer, Toy>();
+        for (String toy: toysList) {
+            ParsingResult parsed = parseToyString(toy);
+            toys.put(parsed.id, new Toy(parsed.name, parsed.weight));
+        }
+    }
+
+    public void put(String toy) {
+        ParsingResult parsed = parseToyString(toy);
+        toys.put(parsed.id, new Toy(parsed.name, parsed.weight));
+    }
+
+    public String get() {
+        ArrayList<Integer> toys_queue = new ArrayList<>();
+        for (Map.Entry<Integer, Toy> entry: toys.entrySet()) {
+            toys_queue.addAll(Collections.nCopies(entry.getValue().weight, entry.getKey()));
+        }
+        Random rand = new Random();
+        return toys.get(toys_queue.get(rand.nextInt(toys_queue.size()))).name;
+    }
+
+    private ParsingResult parseToyString (String str) {
+        String[] split = str.split("\\s+");
+        if (split.length != 3) {
+            throw new RuntimeException("Wrong number of arguments.");
+        }
+        ParsingResult result = new ParsingResult();
+        try {
+            result.id = Integer.parseInt(split[0]);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Failed to parse ID '" + split[0] + "'. Error: " + e.toString());
+        }
+        result.name = split[1];
+        try {
+            Integer weight = Integer.parseInt(split[2]);
+            if (weight < 0 || weight > 100) {
+                throw new RuntimeException("Weight should be a number from interval 1 .. 100");
+            }
+            result.weight = weight;
+        } catch (NumberFormatException e) {
+            String err = "Failed to parse ID '" + split[0] + "'. Error: " + e.toString();
+            throw new RuntimeException(err);
+        }
+
+        return result;
+    }
 }
--- Java/mainClassProject.java
+++ Java/mainClassProject.java
@@ -1,6 +1,23 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
 public class mainClassProject {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome to the Toy Shop.");
+        ToyStore store = new ToyStore(Arrays.asList("1 Ball 6", "2 Chess 2"));
+        store.put("3 BabyDoll 4");
+        store.put("4 Plane 5");
+        store.put("5 Puzzles 7");
+        store.put("6 Cubes 3");
+        store.put("7 TeddyBear 4");
+        store.put("8 Robot 2");
+        BufferedWriter writer = new BufferedWriter(new FileWriter("store_out.txt", false));
+        for (Integer i=0; i < 10; i++) {
+            writer.write(Integer.toString(i + 1) + " : " + store.get() + "\n");
+        }
+        writer.close();
     }
 }
 
